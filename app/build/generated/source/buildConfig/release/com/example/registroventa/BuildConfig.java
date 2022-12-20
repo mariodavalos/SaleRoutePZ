914705e71ebbc622ceb2c3c4b655974ff8e6e6e7
Mario Fernando Davalos Navarro <developers@eurekasigma.com>
2022-12-20 12:27:02 -0600

    5.9.0

---
+++ app/build/generated/source/buildConfig/release/com/example/registroventa/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.registroventa;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "dinamica.ventaenruta";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 577;
+  public static final String VERSION_NAME = "5.7.7";
+}
